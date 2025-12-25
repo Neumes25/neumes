c458a173f43411bfad3b66ef57427d7c5ebb56ac
Matej <matejkostoski123@gmail.com>
2025-12-25 15:56:13 +0100

    Major Update: 1) Function added: Can add new line at begining of document. 2) Function added: Can style font and size of text in new lines. 3) Function added: Can select and move text under notes, some notes, starting martiria. Once selected use the arrowkeys to move. Also, can add the class selected to a any <div> (including any note, text, newline and similar) in the export window and use the arrow keys to move it.4) Reworked martirias.

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,13 @@ function init(){
     countTextNote = 0;
     document.getElementById('textInputArea').value=""
     document.getElementById('neumesInportArea').value=""
-    document.getElementById('titleText').value=""
-    document.getElementById('startMartiria').value=""
+    document.getElementById('startNote').value=""
     document.getElementById('startTempo').value=""
-    document.getElementById('titleTextInput').value=""
     document.getElementById('pageNumberInput').value="1"
     document.getElementById('Upload').value=''
-    document.getElementById('titleText').innerHTML=''
-    document.getElementById('startMartiria').innerHTML=''
+    document.getElementById('startNote').innerHTML=''
     document.getElementById('startTempo').innerHTML=''
+    document.getElementById('newLineFontSize').value = '16'
 
     countTextNote = 0;
 
@@ -26,8 +24,10 @@ function addNoteWithText(note, font){
     newText.classList.add("text")
     newText.innerText=''
     newText.setAttribute("id", countTextNote)
+    newText.setAttribute("onclick", "event.stopPropagation(); choose(this)")
 
     newNote = document.createElement('div')
+    newNote.setAttribute("onclick", "choose(this)")
     newNote.innerHTML=note
     newNote.classList.add(font)
     newNote.classList.add('note')
@@ -46,6 +46,7 @@ function addNoteNoText(note, font){
     newNote.innerHTML=note
     newNote.classList.add(font)
     newNote.classList.add('note')
+    newNote.setAttribute("onclick", "choose(this)")
     paperContent.appendChild(newNote)   
 
 }
@@ -71,6 +72,7 @@ function addMartiria(note, font){
     newMartiria.classList.add(font)
     newMartiria.classList.add('red')
     newMartiria.classList.add('note')
+    newNote.setAttribute("onclick", "choose(this)")
        
     paperContent.appendChild(newMartiria)     
 }
@@ -149,10 +151,36 @@ function newSpace(){
 
 function newLine(){
     
+    newLineFont = document.getElementById('newLineFont').value
+    newLineFontSize = document.getElementById('newLineFontSize').value
+
     line=document.createElement('div')
     line.classList.add('newline');
     line.setAttribute('contenteditable', 'true');
+    line.setAttribute('onclick', "event.stopPropagation(); this.focus(); ")
+
+    if (newLineFont=="MKD"){
+        line.style.fontFamily = 'Calibri';
+    }      
+    else if (newLineFont=="CSL"){
+        line.style.fontFamily = 'Irmologion';
+    }    
+    
+    line.style.fontSize = newLineFontSize + 'pt' 
+
+    if (document.getElementById('newLineRed').checked){
+        line.style.color='#ed0000'
+    }
+
     paperContent.appendChild(line)
+}
+
+function newLineAtStart(){
+    line=document.createElement('div')
+    line.classList.add('newline');
+    line.setAttribute('contenteditable', 'true');
+    line.setAttribute('onclick', "event.stopPropagation(); this.focus(); ")
+    paperContent.insertBefore(line, paperContent.firstChild)
 }
 
 function exportNotes(){
@@ -186,15 +214,6 @@ function ImportNeumes(){
 }
 
 
-function titlePosition(value){
-    document.getElementById('title').style.setProperty('justify-content', value)
-}
-
-function titleOn(value){
-    document.getElementById('title').style.setProperty('display', value)
-}
-
-
 function activeteNoteButtons(button){
     elements = document.getElementsByClassName('Button-menu');
 
@@ -205,17 +224,10 @@ function activeteNoteButtons(button){
 }
 
 
-function addTitle(){
-    textTEMP=document.getElementById("titleTextInput").value
-    document.getElementById("titleText").innerText = textTEMP
-}
-
-
 function addTitleMartira(startingMartiria, position){
-    position = document.getElementById(position);
-    position.innerHTML = ''
-    position.innerHTML = startingMartiria;
 
+    possition = document.getElementById(position)
+    possition.innerHTML = startingMartiria;
 }
 
 
@@ -307,9 +319,52 @@ function copyToClipboard(value){
     navigator.clipboard.writeText(value)
 }
 
+function choose(elements){
+    selectedNotes=document.getElementsByClassName("selected");
+    Array.from(selectedNotes).forEach(selectedNote => {
+        
+        selectedNote.classList.remove("selected")
+    });
+    elements.classList.add("selected")
+}
+
+function moveRight(){
+
+    selectedElement = document.getElementsByClassName("selected")
+
+    if (selectedElement.length == 0) return;
+        selectedElement[0].style.left = parseInt(window.getComputedStyle(selectedElement[0]).left) + 3 + 'px';
+
+}
 
 
+function moveLeft(){
 
+    selectedElement = document.getElementsByClassName("selected")
+
+    if (selectedElement.length == 0) return;
+        selectedElement[0].style.left = parseInt(window.getComputedStyle(selectedElement[0]).left) - 3 + 'px';
+
+}
+
+
+function moveDown(){
+
+    selectedElement = document.getElementsByClassName("selected")
+
+    if (selectedElement.length == 0) return;
+        selectedElement[0].style.top = parseInt(window.getComputedStyle(selectedElement[0]).top) + 3 + 'px';
+
+}
+
+function moveUp(){
+
+    selectedElement = document.getElementsByClassName("selected")
+
+    if (selectedElement.length == 0) return;
+        selectedElement[0].style.top = parseInt(window.getComputedStyle(selectedElement[0]).top) - 3 + 'px';
+
+}
 
 
 
@@ -331,9 +386,18 @@ document.addEventListener('keydown', (event)=>{
    
     document.getElementsByName(event.key.toString())[0]?.click()
 
+
+
+    if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)) event.preventDefault()
+    if (event.key == 'ArrowUp' && document.getElementsByClassName("selected") !=0)  moveUp()
+
+    if (event.key == 'ArrowDown' && document.getElementsByClassName("selected") !=0) moveDown()
+    
+    if (event.key == 'ArrowLeft' && document.getElementsByClassName("selected") !=0) moveLeft()
+
+    if (event.key == 'ArrowRight' && document.getElementsByClassName("selected") !=0) moveRight()
+    
+    if (event.key == "Escape") {
+        document.getElementsByClassName("selected")[0].classList.remove("selected")
+    }
 })
-
-
-
-
-
